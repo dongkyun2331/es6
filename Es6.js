387a8410e78112649dfc7041aeb650baf8ec1f86
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-09 01:57:14 +0900

    var

--- Es6.js
+++ Es6.js
@@ -3,7 +3,7 @@ let age = 20;
 const name = "John";
 
 // arrow functions
-// const array = [1,2,3];
+var array = [1, 2, 3];
 
 // template string
 const greeting = (name) => {
@@ -15,7 +15,7 @@ const greeting = (name) => {
 const add = (a = 2, b = 2) => console.log(a + b);
 
 // array destructuring
-const array = [1, 2, 3];
+var array = [1, 2, 3];
 
 const first = array[0];
 const third = array[2];
